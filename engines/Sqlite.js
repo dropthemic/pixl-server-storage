d0983c6d097c20dcd98e73a5d81bda24a0d8939c
dropthemic <20729448+dropthemic@users.noreply.github.com>
2019-12-23 13:09:52 -0800

    Changed sqlite library to sqlite3 to pass unit tests.

--- engines/Sqlite.js
+++ engines/Sqlite.js
@@ -2,22 +2,22 @@
 // Copyright (c) 2015 - 2018 Joseph Huckaby
 // Released under the MIT License
 
-// Requires the 'better-sqlite3' module from npm
-// npm install better-sqlite3
+// Requires the 'sqlite3' module from npm
+// npm install sqlite3
 
 var Class = require("pixl-class");
 var Component = require("pixl-server/component");
 var Tools = require("pixl-tools");
-var BetterSq = require('better-sqlite3');
+var sqlite3 = require('sqlite3');
 
 module.exports = Class.create({
-	
 	__name: 'Sqlite',
 	__parent: Component,
 	
 	defaultConfig: {
-		connectString: null,
-		keyPrefix: ""
+		connectString: ":memory:",
+		keyPrefix: "",
+		flushWalMinutes: 1
 	},
 	
 	startup: function(callback) {
@@ -30,18 +30,19 @@ module.exports = Class.create({
 		// setup Couchbase connection
 		var self = this;
 		var cstr = this.config.get('connectString');
-		var opt = {};
-		if(!cstr) opt.memory = true;
 		self.logDebug(9, "connectString value => " + cstr);
 		self.keyPrefix = this.config.get('keyPrefix')+"";
-		self.db = BetterSq(cstr, opt);
-		self.db.exec("CREATE TABLE IF NOT EXISTS kv(key TEXT NOT NULL, val BLOB, lastmod int, size int, PRIMARY KEY (key))")
-		self.stmt_put = self.db.prepare("INSERT OR REPLACE INTO KV(key,val,lastmod,size)VALUES(?,?,?,?)")
-		self.stmt_head = self.db.prepare("SELECT lastmod, coalesce(size,length(val)) len FROM kv WHERE key=?")
-		self.stmt_get = self.db.prepare("SELECT val FROM kv WHERE key=?")
-		self.stmt_delete = self.db.prepare("DELETE FROM kv WHERE key=?")
-		self.logDebug(9, "setup completed");
-		if(callback) callback();
+		self.db = new sqlite3.Database(cstr);
+		self.flushWalMinutes = parseFloat(this.config.get('flushWalMinutes'));
+		
+		if(isNaN(self.flushWalMinutes)) self.flushWalMinutes = 0;
+		self.lastFlush = new Date(0);
+		
+		self.db.run("PRAGMA journal_mode=WAL;");
+		self.db.run("CREATE TABLE IF NOT EXISTS kv(key TEXT NOT NULL, val BLOB, lastmod int, size int, PRIMARY KEY (key))", (e) => {
+			callback(e);
+			self.logDebug(9, "setup completed");
+		});
 	},
 	prepKey: function(key) {
 		// prepare key for S3 based on config
@@ -53,7 +54,6 @@ module.exports = Class.create({
 	
 	put: function(key, value, callback) {
 		var self = this;
-		var err = null;
 		//self.logDebug(9, "put key=" + key + " value=" + JSON.stringify(value));
 		key = self.prepKey(key);
 		var sz = null;
@@ -63,14 +63,26 @@ module.exports = Class.create({
 		else {
 			value = JSON.stringify(value)
 		}
-		try {
-			self.stmt_put.run(key, value, (new Date()).getTime(), sz);
+
+		var stmt_put = self.db.prepare("INSERT OR REPLACE INTO KV(key,val,lastmod,size)VALUES(?,?,?,?)");
+		stmt_put.run([key, value, (new Date()).getTime(), sz], function(e) {
+			stmt_put.finalize();
+			if(e) {
+				callback(e);
+			}
+			else self.flushWal(callback);
+		});
+
+	},
+	
+	flushWal: function(callback) {
+		var self = this;
+		//flush cache
+		if(self.flushWalMinutes <= 0 || (((new Date))-self.lastFlush) > self.flushWalMinutes * 60 * 1000) {
+			self.lastFlush = new Date();
+			self.db.run("PRAGMA wal_checkpoint", (e) => { callback(e); });
 		}
-		catch(e) {
-			//self.logDebug(9, "error=" + e);
-			err = e;
-		}
-		if (callback) callback(err);
+		else callback(null);
 	},
 	
 	putStream: function(key, inp, callback) {
@@ -89,35 +101,42 @@ module.exports = Class.create({
 		// head couchbase value given key
 		var self = this;
 		key = self.prepKey(key);
-		var data = self.stmt_head.get(key);
-		if(!data) {
-			var err = new Error("Failed to head key: " + key + ": Not found");
-			err.code = "NoSuchKey";
-			callback( err, null );
-		}
-		else {
-			callback( null, { mod: Math.floor(data.lastmod / 1000), len: data.len }  );
-		}
+		var stmt_head = self.db.prepare("SELECT lastmod, coalesce(size,length(val)) len FROM kv WHERE key=?");
+		stmt_head.get(key, (err, data) => {
+			stmt_head.finalize();
+			if(!data) {
+				var err = new Error("Failed to head key: " + key + ": Not found");
+				err.code = "NoSuchKey";
+				callback( err, null );
+			}
+			else {
+				callback( null, { mod: Math.floor(data.lastmod / 1000), len: data.len }  );
+			}
+		});
 	},
 	
 	get: function(key, callback) {
 		// fetch Couchbase value given key
 		var self = this;
-		this.logDebug(9, "Fetching Object: " + key);
+		//this.logDebug(9, "Fetching Object: " + key);
 		key = this.prepKey(key);
-		var data = self.stmt_get.get(key);
-		if(!data) {
-			var err = new Error("Failed to head key: " + key + ": Not found");
-			err.code = "NoSuchKey";
-			callback( err, null );
-		}
-		else {
-			//this.logDebug(9, "Fetching Object: " + key + " val=" + data.val);
-			var val = data.val;
-			//{"type":"Buffer","data":[71,1]}
-			if(!self.storage.isBinaryKey(key)) val = JSON.parse(val);
-			callback( null, val);
-		}
+		var stmt = self.db.prepare("SELECT val FROM kv WHERE key=?", key)
+		stmt.get(key, (err, data) => {
+			stmt.finalize()
+			if(!data) {
+				var err = new Error("Failed to head key: " + key + ": Not found");
+				err.code = "NoSuchKey";
+				callback( err, null );
+			}
+			else {
+				//this.logDebug(9, "Fetching Object: " + key + " val=" + data.val);
+				var val = data.val;
+				//{"type":"Buffer","data":[71,1]}
+				if(!self.storage.isBinaryKey(key)) val = JSON.parse(val);
+				callback( null, val);
+			}
+			
+		});
 	},
 	
 	getStream: function(key, callback) {
@@ -146,16 +165,23 @@ module.exports = Class.create({
 		this.logDebug(9, "Deleting Object: " + key);
 		key = this.prepKey(key);
 		
-		var data = self.stmt_head.get(key);
-		var err = null;
-		if(!data) {
-			err = new Error("Failed to delete key: " + key + ": Not found");
-			err.code = "NoSuchKey";
-		}
-		else {
-			self.stmt_delete.run(key);
-		}
-		callback(err);
+		var stmt_head = self.db.prepare("SELECT lastmod, coalesce(size,length(val)) len FROM kv WHERE key=?");
+		var data = stmt_head.get(key, (err, data) =>{
+			stmt_head.finalize();
+			if(!data) {
+				err = new Error("Failed to delete key: " + key + ": Not found");
+				err.code = "NoSuchKey";
+				callback(err);
+			}
+			else {
+				var stmt_delete = self.db.prepare("DELETE FROM kv WHERE key=?");
+				stmt_delete.run(key, (e2, d2) => {
+					stmt_delete.finalize();
+					self.flushWal(callback);
+				});
+			
+			}
+		});
 	},
 
 	runMaintenance: function(callback) {
